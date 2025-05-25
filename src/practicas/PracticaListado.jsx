f92727aa9c8efbcd3ff77e9eb62e9bba7ceee7d3
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-05-25 23:00:32 +0200

    agregamos funcion addTask

--- src/practicas/PracticaListado.jsx
+++ src/practicas/PracticaListado.jsx
@@ -1,47 +1,52 @@
 import { useState } from "react"
 
-const ItemsNombres = ({nombre , visto}) => {
+const ItemsNombres = ({ nombre, visto }) => {
   return (
-    <li>{ nombre } 
-    { visto ? <AlertaPositiva /> : <AlertaNegativa /> } 
+    <li>{nombre}
+      {visto ? <AlertaPositiva /> : <AlertaNegativa />}
     </li> // Podemos pasar componentes como ternarios
   )
 }
 
 const AlertaPositiva = () => {
   return (
-   '✅'
+    '✅'
   )
 }
 
 const AlertaNegativa = () => {
-  return( 
+  return (
     '⛔'
-   )
+  )
 }
 
 const listadoNombres = [
-  {nombre : 'Ricardo Ferreira' , visto : true},
-  {nombre : 'Santiago Ferreira' , visto : true},
-  {nombre : 'Asier Ferreira' , visto : true},
-  {nombre : 'Roberto Ferreira' , visto : true},
-  {nombre : 'Eduardo Ferreira' , visto : true},
-  {nombre : 'Anibal Ferreira' , visto : true}
+  { nombre: 'Ricardo Ferreira', visto: true },
+  { nombre: 'Santiago Ferreira', visto: true },
+  { nombre: 'Asier Ferreira', visto: true },
+  { nombre: 'Roberto Ferreira', visto: true },
+  { nombre: 'Eduardo Ferreira', visto: true },
+  { nombre: 'Anibal Ferreira', visto: true }
 ]
+
 
 
 export const PracticaListado = () => {
 
-const [arreglo, setArreglo] = useState(listadoNombres)
+  const [arreglo, setArreglo] = useState(listadoNombres)
+
+  const addTask = () => {
+    setArreglo([...arreglo, { nombre: 'Manuel Ferreira', visto: false }])
+}
 
 
   return (
     <>
       <h1>Practica maps, ternarios</h1>
-
       <ul>
-        { arreglo.map( item => <ItemsNombres key={item.nombre} nombre={item.nombre} visto={item.visto}/>) }
+        {arreglo.map(item => <ItemsNombres key={item.nombre} nombre={item.nombre} visto={item.visto} />)}
       </ul>
+      <button onClick={() => addTask()}>Agregar Tarea</button>
     </>
   )
 }
