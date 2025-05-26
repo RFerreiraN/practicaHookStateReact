f51ac24c8d3ad53b534e72591edb17b3d86a6060
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-05-26 11:50:10 +0200

    agregamos un form para manejar con el submit el evento

--- src/practicas/AgregarTarea.jsx
+++ src/practicas/AgregarTarea.jsx
@@ -1,22 +1,41 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const AgregarTarea = () => {
 
   const [inputValue, setInputValue] = useState('')
 
   const handleInputValue = (event) => {
-      setInputValue(event.target.value)
-      console.log(inputValue)
+    setInputValue(event.target.value)
+    
+  }
+
+
+  const onSubmit = (e) => {
+      e.preventDefault()
+      console.log(e)
   }
 
   return (
     <>
-      <input type="text"
-             placeholder="Ingresa Tarea"
-             value={inputValue}
-             onChange={handleInputValue}
-      />
-    
+    {/* Creamos un input type text para tomar su valor e ingresarlo como nueva tarea, para ellos
+        usamos la prop value, a la cual se le pasara el estado inputValue y tambien se creara el evento
+        onChange el cual tendrá una funcion que permite ir modificando el estado a traves del setInputValue.
+        a esto se denomina (input controlado)
+
+        creamos un formulario el cual nos permite capturar la ejecución del input a traves del onSubmit
+    */}
+      <form onSubmit={onSubmit}>  
+        <input type="text"
+          placeholder="Ingresa tarea"
+          value={inputValue}
+          onChange={handleInputValue}
+        />
+      </form>
+
+
     </>
+
+
   )
 }
+
