1cf32d073c1b6dae540d873a2cd749fa089aadbf
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-05-31 23:03:16 +0200

    practica de useEffect

--- src/components/UsersApp.jsx
+++ src/components/UsersApp.jsx
@@ -1,11 +1,33 @@
-import {useState} from 'react';
+import {useState, useEffect } from 'react';
 
 export const UsersApp = () => {
+
+  const [users, setUsers] = useState([])
+
+  const fetchUsers = async () => {
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        setUsers(data)
+        // console.log(data)
+      }catch (error){
+        console.error(error)
+      }
+  }
+
+  useEffect( () => {
+    fetchUsers()
+  }, [])
+
   return (
     <>
       <h1>Listado de Usuarios: </h1>
       <ul>
-        <li>Ricardo</li>
+        { users.map( user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+      <h2>Alias de Usuarios: </h2>
+      <ul>
+        { users.map( user => <li key={user.id}>{user.username}</li>)}
       </ul>
     </>
   )
