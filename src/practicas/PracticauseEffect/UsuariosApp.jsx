c67646f4066beb36bb95712793d6f1dd737d9554
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-03 23:13:55 +0200

    cambios y practica

--- src/practicas/PracticauseEffect/UsuariosApp.jsx
+++ src/practicas/PracticauseEffect/UsuariosApp.jsx
@@ -27,13 +27,14 @@ export const UsuariosApp = () => {
 
   return (
     <>
+    
       <h1>Datos del Personal: </h1>
       <ul>
-          { usuarios.map(usuario => <li key={usuario.id}>{ usuario.name }</li>)}
+          { endPoint == 'users' ? usuarios.map(usuario => <li key={usuario.id}>{ usuario.name }</li>)
+                                : usuarios.map(usuario => <li key={usuario.id}>{usuario.email}</li>) }
       </ul>
       <button onClick={cambiarEndPoint}>Traer Usuarios</button>
     </>
   )
 }
 
-//En este caso no usamos useEffect() por que estamos llamando la funcion de la API con un boton.
